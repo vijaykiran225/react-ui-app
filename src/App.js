1db3682916c116c21cb1a0a2ae3718b1fdb95d26
Vijay Kiran <vijaykiran225@gmail.com>
2020-08-01 19:00:21 +0530

    move to hashrouter

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import Graph from './components/graph/graph.js';
 import SchoolCricket from './components/schoolcricket/cric.js';
 
 import CORSComponent from './components/cors/cors.js';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import hellow from './components/hellow.js'
 import Tables from './components/tables/table.js';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
+    <HashRouter>
+      <div className="App" basename='/'>
         <Route path="/" component={hellow} exact />
         <Route path="/graph" component={Graph} />
         <Route path="/cricket" component={SchoolCricket} />
         <Route path="/cors" component={CORSComponent} />
         <Route path="/tables" component={Tables} />
       </div>
-    </BrowserRouter>
+    </HashRouter>
 
 
   );
