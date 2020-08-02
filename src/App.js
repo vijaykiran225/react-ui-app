2543b6b131e94cc2745c2eba5e92e03a294fde9c
Vijay Kiran <vijaykiran225@gmail.com>
2020-08-02 19:52:46 +0530

    update HashRouter to App

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import Graph from './components/graph/graph.js';
 import SchoolCricket from './components/schoolcricket/cric.js';
 
 import CORSComponent from './components/cors/cors.js';
-import { HashRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import hellow from './components/hellow.js'
 import Tables from './components/tables/table.js';
 
 function App() {
   return (
-    <HashRouter>
-      <div className="App" basename='/'>
-        <Route path="/" component={hellow} exact />
-        <Route path="/graph" component={Graph} />
-        <Route path="/cricket" component={SchoolCricket} />
-        <Route path="/cors" component={CORSComponent} />
-        <Route path="/tables" component={Tables} />
-      </div>
-    </HashRouter>
+
+    <div className="App" basename='/'>
+      <Route path="/" component={hellow} exact />
+      <Route path="/graph" component={Graph} />
+      <Route path="/cricket" component={SchoolCricket} />
+      <Route path="/cors" component={CORSComponent} />
+      <Route path="/tables" component={Tables} />
+    </div>
+
 
 
   );
